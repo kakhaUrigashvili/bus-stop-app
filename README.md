f3379758208d44e3228e327b98ace652d3231ef5
kurigashvili <kurigashvili@conversantmedia.com>
2019-07-18 09:36:52 -0500

    updated reamdme and compose

---
+++ README.md
@@ -0,0 +1,21 @@
+# Bus Stop App
+
+## Prerequisites 
+- Docker
+
+## Set Up
+
+1. Start using docker compose 
+```
+docker-compose up
+```
+
+2. In other tab, run database migrations and seeds.
+
+```
+docker-compose exec backend npm run migrate:latest && docker-compose exec backend npm run seed:run
+```
+
+Backend should be accessible on http://localhost:3000
+Frontend should be accessible on http://localhost:8080
+
--- backend/database/seeds/01_seed_rides_table.js
+++ backend/database/seeds/01_seed_rides_table.js
@@ -1,12 +1,14 @@
 /* eslint-disable no-param-reassign */
 const csv = require('csvtojson');
 
-exports.seed = knex => csv()
-    .fromFile(`${__dirname}/resources/rides.csv`)
+const table = 'stops';
+
+exports.seed = knex => knex(table).truncate().then(() => csv()
+    .fromFile(`${__dirname}/resources/rides.csv`) // would not scale for large input. used for simplicity
     .then((rows) => {
         rows.forEach((r) => {
             r.routes = `{"${r.routes.split(',').join('","')}"}`;
             r.location = r.location.replace('(', '{').replace(')', '}');
         });
-        return knex.batchInsert('stops', rows);
-    });
+        return knex.batchInsert(table, rows);
+    }));
